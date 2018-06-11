51390c9a660d7f1b83b6c9c8a642217bb53b49b2
Feodor Fitsner <feodor@appveyor.com>
2018-06-11 12:05:36 -0700

    Add warnings

--- load-narrative.ps1
+++ load-narrative.ps1
@@ -2,8 +2,7 @@ $lines = Get-Content .\appveyor.yml
 
 $script:i = 0
 
-function _() {
-    
+function WriteNext ($color) {
     for(;$script:i -lt $lines.Length; $script:i++) {
         if ($lines[$script:i].StartsWith('#')) {
             break
@@ -13,10 +12,18 @@ function _() {
     Write-Host ""
     for(;$script:i -lt $lines.Length; $script:i++) {
         if ($lines[$script:i].StartsWith('#')) {
-            Write-Host $lines[$script:i].Substring(2) -ForegroundColor DarkCyan
+            Write-Host $lines[$script:i].Substring(2) -ForegroundColor $color
         } else {
             break
         }
     }
     Write-Host ""
+}
+
+function _() {
+    WriteNext DarkCyan
+}
+
+function __() {
+    WriteNext Yellow
 }
